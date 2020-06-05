0a0915f7d0cdbf11802b951c3bcba6a566077c43
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-05 10:29:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,3 +5,17 @@ end
 
 # Define your WIN_COMBINATIONS constant
 constant WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win|
+
+    if board[win[0]] == "X" && board[win[1]] == "X" && board[win[2]] == "X"
+      return true
+    elsif board[top_row_win[0]] == "O" && board[top_row_win[1]] == "O" && board[top_row_win[2]] == "O"
+      return true
+    else
+      return false
+    end
+end
+
+
